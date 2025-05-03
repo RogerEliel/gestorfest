ea2a2526b215602a728f1c04a5705bd5e9fd9140
Roger Santana <66125259+RogerEliel@users.noreply.github.com>
2025-05-03 18:41:18 -0300

    Atualizar o index.ts

--- supabase/functions/convites/index.ts
+++ supabase/functions/convites/index.ts
@@ -1,4 +1,5 @@
 
+import { Buffer } from "https://deno.land/std@0.201.0/node/buffer.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.38.4";
 
