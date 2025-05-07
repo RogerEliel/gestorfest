0b4a93646586af091170d4c08b6d48ea11f9e043
RogerEliel <rogersantana.pssoal@gmail.com>
2025-05-07 00:09:23 -0300

    Mesma coisa do último commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-export default {
+// postcss.config.js
+
+module.exports = {
   plugins: {
-    tailwindcss: {},
+    // Se você ainda usa importações em CSS, mantenha postcss-import:
+    'postcss-import': {},
+    // Agora o plugin Tailwind via pacote separado:
+    '@tailwindcss/postcss': {},
+    // Autoprefixer continua opcional
     autoprefixer: {},
   },
-}
+};
